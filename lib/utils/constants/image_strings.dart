2324240df3c15e71e6b72826ef1eff96da833bb2
FuzionOrigins <nieznam2016@gmail.com>
2024-03-20 01:55:09 +0100

    Added splash screen

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,6 +1,5 @@
 class ImageStrings {
-  static const String darkAppLogo = "assets/logos/splash-logo-dark.png";
-  static const String lightAppLogo = "assets/logos/splash-logo-light.png";
+  static const String appLogo = "assets/logos/splash-logo.png";
 
   static const String google = "assets/logos/google-icon.png";
   static const String facebook = "assets/logos/facebook-icon.png";
