4ac7f8d28bec02242c98f72fd4de37b660f64b5b
Adam Dybcio <adam.dybcio.kontakt@gmail.com>
2024-04-14 02:48:42 +0200

    Added and configured Firebase for this project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:ecommerce_store/app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  //AuthenticationRepository()
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then((value) => Get.put(null));
+
   runApp(const App());
 }
